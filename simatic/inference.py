84319bdf72aa38af679135b6c410f39b960a89ce
p1utoze <1ds21ai001@dsce.edu.in>
2024-07-17 14:39:47 +0530

    update: add speech to text subcommand to main command

--- simatic/inference.py
+++ simatic/inference.py
@@ -1,6 +1,7 @@
 import click
 from simatic.commands import text_gen
 from simatic.commands import speech_gen
+from simatic.commands import speech_rec
 
 
 @click.group()
@@ -8,8 +9,9 @@ def cli():
     pass
 
 
-cli.add_command(text_gen)
-cli.add_command(speech_gen)
+cli.add_command(text_gen, name="chat")
+cli.add_command(speech_gen, name="speak")
+cli.add_command(speech_rec, name="listen")
 
 if __name__ == '__main__':
     cli()
