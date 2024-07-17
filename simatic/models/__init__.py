24322d9727359b109523e7c0b1084e1c6f27129c
p1utoze <1ds21ai001@dsce.edu.in>
2024-07-17 14:38:52 +0530

    update: initialize instace of Speech Recognition class

--- simatic/models/__init__.py
+++ simatic/models/__init__.py
@@ -1,4 +1,6 @@
 from .text import SimaticBaseModel
+from .speech import AudioRecorder
 
 simatic_text = SimaticBaseModel()
-__all__ = ["simatic_text"]
+audio_recorder = AudioRecorder(model_size_or_path="base.en", device="cpu", compute_type="int8_float32")
+__all__ = ["simatic_text", "audio_recorder"]
