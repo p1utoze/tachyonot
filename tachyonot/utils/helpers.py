91629179dfafc786b9ede10f32e4fc65c62d7aaf
p1utoze <1ds21ai001@dsce.edu.in>
2024-09-01 20:11:09 +0530

    update: implement tik token environment setting variable

--- tachyonot/utils/helpers.py
+++ tachyonot/utils/helpers.py
@@ -1,6 +1,15 @@
 import os
 import pywhispercpp.constants as constants
+from pathlib import Path
 
+
+def set_tiktoken_env():
+    """
+    Set environment variables from argparse
+    """
+    tiktoken_dir = Path(__file__).parents[2].absolute() / ".tiktoken_cache"
+    os.environ["TIKTOKEN_CACHE_DIR"] = str(tiktoken_dir)
+    print(os.environ["TIKTOKEN_CACHE_DIR"])
 
 def _get_params(args) -> dict:
     """
