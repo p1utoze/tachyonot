ab2e8d3c2412fc2c9709c4d0cc820f0921039366
p1utoze <1ds21ai001@dsce.edu.in>
2024-09-01 19:53:02 +0530

    Fix: change the stream argument to boolean flag; Update: arguments passed to the store documents command

--- tachyonot/commands/chat.py
+++ tachyonot/commands/chat.py
@@ -1,6 +1,5 @@
-from simatic.models.llama import SimaticLLM
-from simatic.utils import get_config_path
-
+from tachyonot.models.llama import SimaticLLM
+from tachyonot.utils import get_config_path
 chain = SimaticLLM(config_path=get_config_path())
 
 
@@ -14,23 +13,23 @@ def invoke(parser):
     parser.add_argument(
         "--data-path", default=None, help="Path to the data folder for RAG"
     )
-    parser.add_argument("--stream", default=True, help="Enable streaming output")
+    parser.add_argument("--stream", action="store_true", help="Enable streaming output")
 
 
 def run_chat(args):
     """
     Run the chat command
     :param args: LLM chat arugments such as prompt, data_path, stream
-    """
+   self """
     if args.data_path:
-        chain.store_documents(args.data_path)
+        chain.store_documents(data_path=args.data_path)
 
     generator = chain.invoke(args.prompt, stream=args.stream)
 
     CYAN = "\033[96m"
     RESET = "\033[0m"
     print(f"{CYAN}Question: {args.prompt}{RESET}", flush=True)
-    print(f"{CYAN}Response: {RESET}", end="", flush=True)
+    print(f"{CYAN}Response: {RESET}", flush=True)
     for message in generator:
         print(f"{CYAN}{message}{RESET}", end="", flush=True)
     print()
